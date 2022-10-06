718b4c8dcced22070fa9ac6a7681276dbe3125ad
haikelfazzani <haikel.fazzani@hotmail.com>
2022-10-06 21:45:12 +0100

    2.0.5

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ mkdir -p dist \
 && cp -r src/permission dist \
 && cp -r LICENSE dist \
 && cp -r src/icons dist \
+&& mkdir -p dist/popup \
 && cp -r src/popup/index.html dist/popup \
 && cp -r src/popup/record.svg dist/popup \
 && cp -r src/popup/style.css dist/popup
