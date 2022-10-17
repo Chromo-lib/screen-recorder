2734191a37d658e0a4b373d030513c17b0c8001c
haikelfazzani <haikel.fazzani@hotmail.com>
2022-10-17 02:28:16 +0100

    2.1.2

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,6 @@
-ScrollReveal().reveal(document.querySelectorAll('h1'), { duration: 800, reset: true });
-ScrollReveal().reveal(document.querySelectorAll('h2'), { duration: 800, reset: true });
+ScrollReveal().reveal(document.querySelectorAll('h1'), { duration: 800 });
+ScrollReveal().reveal(document.querySelectorAll('h2'), { duration: 800 });
 ScrollReveal().reveal(document.querySelectorAll('.lead'), { duration: 800 });
-ScrollReveal().reveal(document.querySelectorAll('img'), { duration: 800, reset: true });
-ScrollReveal().reveal(document.querySelectorAll('ul'), { duration: 800, reset: true });
-ScrollReveal().reveal(document.querySelectorAll('li'), { interval: 150, reset: true });
+ScrollReveal().reveal(document.querySelectorAll('img'), { duration: 800 });
+ScrollReveal().reveal(document.querySelectorAll('ul'), { duration: 800 });
+ScrollReveal().reveal(document.querySelectorAll('li'), { interval: 150 });
